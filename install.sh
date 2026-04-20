e3a00e8230ca354aaaf098760d0f6c5167e1975a
Leonardos Zinon Chatzigiachtsidis <leonardoszenon@gmail.com>
2026-04-21 01:31:03 +0300

    bugfix grep for bashrc duplication check

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ cp "./$SCRIPT_NAME" "$TARGET"
 chmod +x "$TARGET"
 
 # avoid duplicate injection
-if ! grep -q "/# >>> ssh-matrix-idle >>>" "$HOME/.bashrc"; then
+if ! grep -qF "# >>> ssh-matrix-idle >>>" "$HOME/.bashrc"; then
 cat <<'EOF' >> "$HOME/.bashrc"
 
 # >>> ssh-matrix-idle >>>
