8cb70d42adcde2b8e7f74e15b882f936ef24c5cc
Leonardos Zinon Chatzigiachtsidis <leonardoszenon@gmail.com>
2026-04-20 23:58:42 +0300

    matrix script

---
+++ ssh-matrix-idle.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+TTY="$1"
+[ -e "$TTY" ] || exit 1
+
+IDLE=300
+CHECK=10
+RUNNING=0
+
+while true; do
+        LAST=$(stat -c %X "$TTY" 2>/dev/null) || {
+                sleep "$CHECK"
+                continue
+        }
+
+        NOW=$(date +%s)
+        DIFF=$((NOW-LAST))
+
+        if [ "$DIFF" -ge "$IDLE" ] && [ "$RUNNING" -eq 0 ]; then
+                RUNNING=1
+
+                # run cmatrix
+                docker run --rm -it leonardoszenon/cmatrix:latest < "$TTY" > "$TTY"
+                printf '\n' > "$TTY"
+
+                RUNNING=0
+        fi
+
+        sleep "$CHECK"
+done
