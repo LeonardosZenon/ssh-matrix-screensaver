2b2191a404ddf9d1d9ffc964cabff2e094c6a5d2
Leonardos Zinon Chatzigiachtsidis <leonardoszenon@gmail.com>
2026-04-21 00:40:27 +0300

    bugfix in install.sh

--- install.sh
+++ install.sh
@@ -16,8 +16,8 @@ if ! grep -q "/# >>> ssh-matrix-idle >>>" "$HOME/.bashrc"; then
 cat <<'EOF' >> "$HOME/.bashrc"
 
 # >>> ssh-matrix-idle >>>
-if [[ -n "$SSH_TTY" ]]; then
-    pgrep -f ssh-matrix-idle.sh >/dev/null || ~/.local/bin/ssh-matrix-idle.sh "$SSH_TTY" >/dev/null 2>&1 &
+if [[ -n "$SSH_CONNECTION" && -n "$SSH_TTY" ]]; then
+    ~/.local/bin/ssh-matrix-idle.sh "$SSH_TTY" >/dev/null 2>&1 &
 fi
 # <<< ssh-matrix-idle <<<
 EOF
