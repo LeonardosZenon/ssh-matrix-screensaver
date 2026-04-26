9c00fca74334a879a6c7ad9bb0b0349dc3421038
Leonardos Zinon Chatzigiachtsidis <leonardoszenon@gmail.com>
2026-04-26 10:39:45 +0300

    rollback

--- ssh-matrix-idle.sh
+++ ssh-matrix-idle.sh
@@ -1,59 +1,30 @@
 #!/usr/bin/env bash
 
-TTY="${1:-$SSH_TTY}"
+TTY="$1"
+[ -e "$TTY" ] || exit 1
 
-IDLE=20
+IDLE=300
 CHECK=2
 RUNNING=0
 
-run_native() {
-    cmatrix -r -s -u 10 < "$TTY" > "$TTY" 2>&1
-}
-
-run_docker() {
-    docker run --rm -it \
-        -v "$TTY":"$TTY" \
-        leonardoszenon/cmatrix:latest \
-        < "$TTY" > "$TTY" 2>&1
-}
-
-run_screensaver() {
-
-    # native cmatrix
-    if command -v cmatrix >/dev/null 2>&1; then
-        run_native
-        return
-    fi
-
-    # fallback docker
-    if command -v docker >/dev/null 2>&1; then
-        if run_docker; then
-            return
-        fi
-    fi
-
-
-}
-
 while true; do
-    LAST=$(stat -c %X "$TTY" 2>/dev/null) || {
+        LAST=$(stat -c %X "$TTY" 2>/dev/null) || {
+                sleep "$CHECK"
+                continue
+        }
+
+        NOW=$(date +%s)
+        DIFF=$((NOW-LAST))
+
+        if [ "$DIFF" -ge "$IDLE" ] && [ "$RUNNING" -eq 0 ]; then
+                RUNNING=1
+
+                # run cmatrix
+                docker run -it leonardoszenon/cmatrix:latest < /dev/tty > /dev/tty
+                printf '\n' > /dev/tty
+
+                RUNNING=0
+        fi
+
         sleep "$CHECK"
-        continue
-    }
-
-    NOW=$(date +%s)
-    DIFF=$((NOW - LAST))
-
-    if [ "$DIFF" -ge "$IDLE" ] && [ "$RUNNING" -eq 0 ]; then
-        RUNNING=1
-
-        run_screensaver
-
-        # restore terminal after cmatrix/docker exits
-#        reset < "$TTY" > "$TTY" 2>&1
-
-        RUNNING=0
-    fi
-
-    sleep "$CHECK"
 done
