4b5df139ec8f0033de7321c21d604c435d7b0cee
Leonardos Zinon Chatzigiachtsidis <leonardoszenon@gmail.com>
2026-04-26 09:53:24 +0300

    testing

--- ssh-matrix-idle.sh
+++ ssh-matrix-idle.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+TTY="$1"
+
 IDLE=20
 CHECK=2
 RUNNING=0
@@ -16,12 +18,12 @@ while true; do
         if [ "$DIFF" -ge "$IDLE" ] && [ "$RUNNING" -eq 0 ]; then
                 RUNNING=1
 
-                 run cmatrix docker mode
-                 docker run --rm -it leonardoszenon/cmatrix:latest < /dev/tty > /dev/tty
-                 printf '\n' > /dev/tty
+#                 run cmatrix docker mode
+#                 docker run --rm -it leonardoszenon/cmatrix:latest < /dev/tty > /dev/tty
+#                 printf '\n' > /dev/tty
 
                 # run cmatrix command mode
-#                cmatrix -r -s -u 10
+                cmatrix -r -s -u 10 < /dev/tty > /dev/tty
 
                 RUNNING=0
         fi
